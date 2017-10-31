02220dbd8f26f4bfa752ccec3d64119c5725a79f
masaniwa <masaniwasdp@gmail.com>
2017-11-01 02:01:56 +0900

    Skribis komentojn por esceptoj.

--- source/desktopdial/sdl/exception.d
+++ source/desktopdial/sdl/exception.d
@@ -1,7 +1,15 @@
+/**
+  Modulo, kiu provizas la esceptojn de la SDL-biblioteko.
+
+  Date: 2017/11/1
+  Author: masaniwa
+ */
+
 module desktopdial.sdl.exception;
 
 import std.exception : basicExceptionCtors;
 
+/** Escepto de la SDL-biblioteko. */
 class SDLException : Exception
 {
     mixin basicExceptionCtors;

ad5378f16dd967f8f35c751adff0265155e99385
masaniwa <masaniwasdp@gmail.com>
2017-11-01 02:20:44 +0900

    Skribis komentojn por fenestro.

--- source/desktopdial/sdl/window.d
+++ source/desktopdial/sdl/window.d
@@ -1,3 +1,10 @@
+/**
+  Modulo, kiu provizas strukturon de fenestro.
+
+  Date: 2017/11/1
+  Author: masaniwa
+ */
+
 module desktopdial.sdl.window;
 
 import std.conv : to;
@@ -13,10 +20,22 @@ import derelict.sdl2.sdl :
 
 import desktopdial.sdl.exception : SDLException;
 
+/** Strukturo, kiu administras fenestran rimedon. */
 struct Window
 {
     alias get this;
 
+    /**
+      Konstruas la strukturon kaj fenestron.
+
+      Params:
+        name = La nomo por doni la fenestron.
+        width = La larĝo de la fenestro.
+        height = La alceto de la fenestro.
+
+      Throws:
+        SDLException Kiam konstruado malsukcesas.
+     */
     this(in string name, in int width, in int height)
     {
         data = SDL_CreateWindow(
@@ -37,12 +56,17 @@ struct Window
         data.SDL_DestroyWindow;
     }
 
+    /**
+      Akiras la fenestran rimedon, kiu estas administrata.
+
+      Returns: La fenestra rimedo.
+     */
     SDL_Window* get() pure nothrow @nogc @safe
     {
         return data;
     }
 
-    private SDL_Window* data;
+    private SDL_Window* data; /// Fenestra rimedo, kiu estas administrata.
 
     invariant
     {

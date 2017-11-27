033a1c8d08966744f500f7fa57251c8f3aa72836
masaniwa <masaniwasdp@gmail.com>
2017-11-28 00:52:15 +0900

    Aldonis komentojn.

--- source/desktopdial/graphic/hands.d
+++ source/desktopdial/graphic/hands.d
@@ -1,3 +1,10 @@
+/**
+  Modulo, kiu provizas manojn de horloĝo.
+
+  Copyright: 2017 masaniwa
+  License: MIT
+ */
+
 module desktopdial.graphic.hands;
 
 import derelict.sdl2.sdl : SDL_Color, SDL_FLIP_NONE, SDL_GetRendererOutputSize, SDL_Rect, SDL_RenderCopyEx;
@@ -8,8 +15,19 @@ import desktopdial.sdl.texture : Texture;
 import std.datetime : SysTime;
 import std.typecons : Tuple, tuple;
 
+/** Manoj de horloĝo. Uzi tiojn postulas la SDL-bibliotekon. */
 struct Hands
 {
+    /**
+      Konstruas la manojn.
+
+      Params:
+        renderer = Rendisto uzota por konstrui la manojn.
+        visual = La vido de la manoj.
+
+      Throws:
+        desktopdial.sdl.exception.SDLException Kiam konstruado malsukcesas.
+     */
     this(ref Renderer renderer, in HandsVisual visual)
     {
         hour = renderer.drawHand(visual.hour);
@@ -19,6 +37,13 @@ struct Hands
 
     this(this) @disable;
 
+    /**
+      Desegnas la manojn en fenestro.
+
+      Params:
+        renderer = Rendisto de fenestro uzota por desegni.
+        time = Tempo, kiu estos montrata.
+     */
     void draw(ref Renderer renderer, in SysTime time) nothrow
     {
         immutable angles = time.calcAngles;
@@ -28,28 +53,42 @@ struct Hands
         renderer.SDL_RenderCopyEx(second, null, null, angles[2], null, SDL_FLIP_NONE);
     }
 
-    private Texture hour;
-    private Texture minute;
-    private Texture second;
+    private Texture hour;   /// Teksturo de horoj manoj.
+    private Texture minute; /// Teksturo de minutoj manoj.
+    private Texture second; /// Teksturo de sekundoj manoj.
 }
 
+/** Vidoj de horloĝaj manoj. */
 struct HandsVisual
 {
-    HandVisual hour;
-    HandVisual minute;
-    HandVisual second;
+    HandVisual hour;   /// Vido de horoj manoj.
+    HandVisual minute; /// Vido de minutoj manoj.
+    HandVisual second; /// Vido de sekundoj manoj.
 }
 
+/** Vido de horloĝa mano. */
 struct HandVisual
 {
-    ushort width;
-    ushort longer;
-    ushort shorter;
+    ushort width;   /// La larĝo de mano.
+    ushort longer;  /// La pli longa longo de mano.
+    ushort shorter; /// La pli mallonga longo de mano.
 
-    SDL_Color color;
-    SDL_Color alpha;
+    SDL_Color color; /// Koloro de mano.
+    SDL_Color alpha; /// Travidebla koloro de teksturo.
 }
 
+/**
+  Desegnas manon en teksturo.
+
+  Params:
+    renderer = Rendisto uzota por desegni manon.
+    visual = La vido de simbolo.
+
+  Returns: Teksturo sur kiu desegnis la manon.
+
+  Throws:
+    desktopdial.sdl.exception.SDLException Kiam malsukcesas desegni.
+ */
 private Texture drawHand(ref Renderer renderer, in HandVisual visual)
 {
     int width, height;
@@ -69,6 +108,14 @@ private Texture drawHand(ref Renderer renderer, in HandVisual visual)
     return Texture(renderer, surface);
 }
 
+/**
+  Kalkuras manajn angulojn, kiuj montras tempo donita.
+
+  Params:
+    time = Tempo, kiu estos montrata.
+
+  Returns: Anguloj de la horo, la minuto kaj la sekundo.
+ */
 private Tuple!(double, double, double) calcAngles(in SysTime time) nothrow @safe
 {
     immutable second = time.second + time.fracSecs.total!`msecs` / 1000.0;

e85d4780412b364e2113568d281b27d944fce9bd
masaniwa <masaniwasdp@gmail.com>
2018-03-14 15:23:12 +0900

    Komentis type.d.

--- source/desktopdial/parts/types.d
+++ source/desktopdial/parts/types.d
@@ -1,14 +1,23 @@
+/**
+  Provizas tipojn.
+
+  Authors:   masaniwa
+  Copyright: 2018 masaniwa
+  License:   MIT
+ */
 module desktopdial.parts.types;
 
+/** Strukturo de grandeco. */
 struct Size
 {
-    int w;
-    int h;
+    int w; /// Larĝo.
+    int h; /// Alteco.
 }
 
+/** Strukturo de koloro. */
 struct Color
 {
-    ubyte r;
-    ubyte g;
-    ubyte b;
+    ubyte r; /// Ruĝo.
+    ubyte g; /// Verdo.
+    ubyte b; /// Bluo.
 }
